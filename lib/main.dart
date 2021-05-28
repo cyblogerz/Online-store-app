f975dc392d4dc7bff0d188a7ea982d00e08e5a80
cyblogerz <pranavajay2002@gmail.com>
2021-05-28 21:36:45 +0530

    Added text field, login button

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/screens/loginpage.dart';
 import 'screens/homepage.dart';
 import 'screens/loginpage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,11 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+      ),
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -4,16 +4,50 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Login Page',
+        body: Column(
+      children: <Widget>[
+        Image.asset(
+          'images/login_image.png',
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        Text(
+          'Welcome',
           style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.blue,
+            fontSize: 22,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
-    );
+        SizedBox(
+          height: 20.0,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter UserName', labelText: 'UserName'),
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: 'Enter Password', labelText: 'Password'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                  child: Text('Login'))
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
